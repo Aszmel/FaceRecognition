f838c4d6e895169c12c3773b13b7c6d66cac954f
Aszmel <aszmel@o2.pl>
2018-09-27 22:24:32 +0200

    small names changes

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -4,7 +4,7 @@ const Rank = () => {
   return (
     <div>
       <div className="white f3">{"Arek, your current rank is..."}</div>
-      <div className="white f1">{"#5"}</div>
+      <div className="white f1">{"#0"}</div>
     </div>
   );
 };
